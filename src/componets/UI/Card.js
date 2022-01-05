504d99ff9d49be7fe99e68464c86908aa1400138
ALAM-97 <alexamara97@gmail.com>
2022-01-05 12:29:37 +0100

    Lesson 10

--- src/componets/UI/Card.js
+++ src/componets/UI/Card.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Card.css';
 
-function Card(props) {
-    // props.className prende le classi presenti in ExpenseItem all'interno del componente Card
-    const classes = 'card ' + props.className
+const Card = (props) => {
+  // props.className prende le classi presenti in ExpenseItem all'interno del componente Card
+  const classes = 'card ' + props.className
 
-    return (
-        <div className={ classes }>{ props.children }</div>
-    );
+  return (
+    <div className={ classes }>{ props.children }</div>
+  );
 }
 
 export default Card;
